9420d15b46bb03da5120d97d513d00113d5be735
David Molineus <david.molineus@netzmacht.de>
2015-10-17 20:02:57 +0200

    Update translations.

--- module/languages/de/default.php
+++ module/languages/de/default.php
@@ -3,7 +3,9 @@
 /*
  * content elements
  */
-$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart'][0] = 'Gruppe Anfang';
-$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart'][1] = 'Gruppiert einzelne Akkordeon Elemente, sodass nur ein Element offen sein kann.';
-$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd'][0]   = 'Gruppe Ende';
-$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd'][1]   = 'Ende einer Akkordeongruppe';
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd']['0']   = 'Akkordeon-Gruppe Ende';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd']['1']   = 'Ende einer Akkordeongruppe';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart']['0'] = 'Akkordeon-Gruppe Anfang';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart']['1'] = 'Gruppiert einzelne Akkordeon Elemente und erzwingt ein geöffnetes Element.';
+
---
+++ module/languages/de/tl_content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-04-17T16:01:05+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_content']['bootstrap_collapseIn']['0'] = 'Standardmäßig öffnen';
+$GLOBALS['TL_LANG']['tl_content']['bootstrap_collapseIn']['1'] = 'Dieses Panel ist beim Laden der Seite geöffnet.';
+
