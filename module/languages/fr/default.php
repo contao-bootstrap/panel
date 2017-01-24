c7f5d484f64f68cbd26fe8f1e19ceae491ba9d5e
David Molineus <david.molineus@netzmacht.de>
2017-01-24 13:48:00 +0100

    Update translations.

---
+++ module/languages/fr/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2017-01-24T13:46:47+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd']['0']   = 'Fin d\'accordéon';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupEnd']['1']   = 'Annonce la fin d\'un accordéon';
+$GLOBALS['TL_LANG']['CTE']['bootstrap_accordionGroupStart']['0'] = 'Début d\'accordéon';
+
