d8b81ed79db271741131f545e6bda50f0d3e65ab
David Molineus <david.molineus@netzmacht.de>
2018-06-14 16:27:15 +0200

    Add basice bundle structure.

---
+++ src/DependencyInjection/ContaoBootstrapPanelExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Contao Bootstrap panel.
+ *
+ * @package    contao-bootstrap
+ * @subpackage Panel
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @license    https://github.com/contao-bootstrap/panel/blob/master/LICENSE LGPL 3.0
+ * @filesource
+ */
+
+declare(strict_types=1);
+
+namespace ContaoBootstrap\Panel\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class ContaoBootstrapPanelExtension.
+ *
+ * @package ContaoBootstrap\Panel\DependencyInjection
+ */
+class ContaoBootstrapPanelExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+        $loader->load('listeners.yml');
+    }
+}
