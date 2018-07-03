4e74d269342e045a97d6037e8caecac80843c01d
David Molineus <david.molineus@netzmacht.de>
2018-07-03 15:39:54 +0200

    Update translations.

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2018-07-02T09:38:55+00:00
+ */
+
+
+$GLOBALS['TL_LANG']['CTE']['bs_panel']                  = 'Panel';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_end']['0']         = 'Panel-Ende';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_end']['1']         = 'Panel-Endelement';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_group_end']['0']   = 'Panelgruppen-Ende';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_group_end']['1']   = 'Panelgruppen-Endelement';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_group_start']['0'] = 'Panelgruppen-Start';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_group_start']['1'] = 'Panelgruppen-Startelement';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_start']['0']       = 'Panel-Start';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_start']['1']       = 'Panel-Startelement';
+
