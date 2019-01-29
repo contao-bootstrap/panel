e5f7cb36e2730029765fbc8434ea5313f0dfe240
David Molineus <david.molineus@netzmacht.de>
2019-01-29 08:30:04 +0100

    Introduce AbstractPanelStartElement to avoid duplication.

--- src/Components/ContentElement/PanelSingleElement.php
+++ src/Components/ContentElement/PanelSingleElement.php
@@ -6,7 +6,6 @@
  * @package    contao-bootstrap
  * @subpackage Panel
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @author     Alexander Kehr <info@kehr-solutions.de>
  * @copyright  2014-2018 netzmacht David Molineus. All rights reserved.
  * @license    https://github.com/contao-bootstrap/panel/blob/master/LICENSE LGPL 3.0-or-later
  * @filesource
@@ -16,12 +15,10 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Panel\Components\ContentElement;
 
-use Contao\StringUtil;
-
 /**
  * Class PanelGroupStartElement
  */
-final class PanelSingleElement extends AbstractPanelElement
+final class PanelSingleElement extends AbstractPanelStartElement
 {
     /**
      * Template name.
@@ -29,43 +26,4 @@ final class PanelSingleElement extends AbstractPanelElement
      * @var string
      */
     protected $templateName = 'ce_bs_panel_single';
-
-    /**
-     * {@inheritdoc}
-     */
-    /**
-     * {@inheritDoc}
-     */
-    protected function prepareTemplateData(array $data): array
-    {
-        $data  = parent::prepareTemplateData($data);
-        $cssId = $data['cssId'] ?: 'panel-' . $this->get('id');
-
-        $data['expanded']   = (bool) $this->get('bs_expanded');
-        $data['headingId']  = $cssId . '-heading';
-        $data['collapseId'] = $cssId . '-collapse';
-        $data['groupId']    = $this->getGroupId();
-
-        return $data;
-    }
-
-    /**
-     * Get the panel group id.
-     *
-     * @return string
-     */
-    private function getGroupId(): ?string
-    {
-        $group = $this->getPanelGroup();
-        if (!$group) {
-            return null;
-        }
-
-        $cssID = StringUtil::deserialize($group->cssID, true);
-        if ($cssID[0] != '') {
-            return (string) $cssID[0];
-        }
-
-        return 'panel-group-' . $group->id;
-    }
 }
--- src/Components/ContentElement/PanelStartElement.php
+++ src/Components/ContentElement/PanelStartElement.php
@@ -15,12 +15,10 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Panel\Components\ContentElement;
 
-use Contao\StringUtil;
-
 /**
  * Class PanelStartElement
  */
-final class PanelStartElement extends AbstractPanelElement
+final class PanelStartElement extends AbstractPanelStartElement
 {
     /**
      * Template name.
@@ -28,40 +26,4 @@ final class PanelStartElement extends AbstractPanelElement
      * @var string
      */
     protected $templateName = 'ce_bs_panel_start';
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function prepareTemplateData(array $data): array
-    {
-        $data  = parent::prepareTemplateData($data);
-        $cssId = $data['cssId'] ?: 'panel-' . $this->get('id');
-
-        $data['expanded']   = (bool) $this->get('bs_expanded');
-        $data['headingId']  = $cssId . '-heading';
-        $data['collapseId'] = $cssId . '-collapse';
-        $data['groupId']    = $this->getGroupId();
-
-        return $data;
-    }
-
-    /**
-     * Get the panel group id.
-     *
-     * @return string
-     */
-    private function getGroupId(): ?string
-    {
-        $group = $this->getPanelGroup();
-        if (!$group) {
-            return null;
-        }
-
-        $cssID = StringUtil::deserialize($group->cssID, true);
-        if ($cssID[0] != '') {
-            return (string) $cssID[0];
-        }
-
-        return 'panel-group-' . $group->id;
-    }
 }
