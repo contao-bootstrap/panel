861ceb1a496367686ae25eeb3d48b06f83ce2939
David Molineus <david.molineus@netzmacht.de>
2022-10-17 10:19:48 +0200

    Update translations

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -8,9 +8,8 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2018-07-02T09:38:55+00:00
+ * last-updated: 2018-07-03T13:42:48+00:00
  */
-
 
 $GLOBALS['TL_LANG']['CTE']['bs_panel']                  = 'Panel';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_end']['0']         = 'Panel-Ende';
@@ -19,6 +18,8 @@ $GLOBALS['TL_LANG']['CTE']['bs_panel_group_end']['0']   = 'Panelgruppen-Ende';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_group_end']['1']   = 'Panelgruppen-Endelement';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_group_start']['0'] = 'Panelgruppen-Start';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_group_start']['1'] = 'Panelgruppen-Startelement';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_single']['0']      = 'Panel-Einzelelement';
+$GLOBALS['TL_LANG']['CTE']['bs_panel_single']['1']      = 'Panel-Einzelelement';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_start']['0']       = 'Panel-Start';
 $GLOBALS['TL_LANG']['CTE']['bs_panel_start']['1']       = 'Panel-Startelement';
 
